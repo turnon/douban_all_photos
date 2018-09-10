867bf9f9bf702dff88b47b9f42ad9b5c37566a83
ken <block24block@gmail.com>
2018-09-11 00:04:39 +0800

    avoid global variable

--- douban_all_photos.js
+++ douban_all_photos.js
@@ -4,7 +4,7 @@
 // ==UserScript==
 // @name         douban-all-photos
 // @namespace    https://github.com/turnon/douban_all_photos
-// @version      0.0.1
+// @version      0.0.2
 // @description  douban_all_photos
 // @author       block24block@gmail.com
 // @match       *://movie.douban.com/*/photos/
@@ -23,7 +23,7 @@
     }
 
     (async function () {
-      for (loader of page_loaders) {
+      for (var loader of page_loaders) {
         var data = await loader
         config.append_page(data, $)
       }
