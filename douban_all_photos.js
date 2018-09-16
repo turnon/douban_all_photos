4e0e0eefd3dd95d23f60fb9ec489bcaf729f3dd1
ken <block24block@gmail.com>
2018-09-16 16:26:31 +0800

    use ateles to load pages_loader

--- douban_all_photos.js
+++ douban_all_photos.js
@@ -4,29 +4,31 @@
 // ==UserScript==
 // @name         douban-all-photos
 // @namespace    https://github.com/turnon/douban_all_photos
-// @version      0.0.8
+// @version      0.1.0
 // @description  douban_all_photos
 // @author       block24block@gmail.com
 // @match       *://movie.douban.com/*/photos*
 // @grant        none
-// @require https://greasyfork.org/scripts/372117-mypagesloader/code/MyPagesLoader.js?version=628225
+// @require https://greasyfork.org/scripts/372188-ateles/code/ateles.js?version=629416
 // ==/UserScript==
-MyPagesLoader.config(jQuery, {
-  page_count: function ($) {
-    return parseInt($('.paginator .next').prev().text())
-  },
-  next_page: function (n) {
-    var start = (n - 1) * 30
-    return window.location.href + '/?type=C&sortby=like&size=a&subtype=a&start=' + start
-  },
-  append_page: function (data, $) {
-    var $article = $(data).find(".article")
-    $article.find(".opt-bar-line").remove()
-    $(".article").last().after($article)
-  },
-  button: function ($) {
-    var $btn = $('<span>&gt;&gt;&gt;all</span>')
-    $('.paginator .next').after($btn)
-    return $btn
-  }
+Ateles(['page_loader'], function (page_loader) {
+  page_loader(jQuery, {
+    page_count: function ($) {
+      return parseInt($('.paginator .next').prev().text())
+    },
+    next_page: function (n) {
+      var start = (n - 1) * 30
+      return window.location.href + '/?type=C&sortby=like&size=a&subtype=a&start=' + start
+    },
+    append_page: function (data, $) {
+      var $article = $(data).find(".article")
+      $article.find(".opt-bar-line").remove()
+      $(".article").last().after($article)
+    },
+    button: function ($) {
+      var $btn = $('<span>&gt;&gt;&gt;all</span>')
+      $('.paginator .next').after($btn)
+      return $btn
+    }
+  })
 })
